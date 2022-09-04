6ecad2684edbd6fadf494c648e57774b9c19846a
Achedon12 <leoderoinleo@gmail.com>
2022-09-04 17:22:01 +0200

    add jobs

--- scripts/Job.js
+++ scripts/Job.js
@@ -1,8 +1,45 @@
-import { world } from "mojang-minecraft";
+import { world, Player } from "mojang-minecraft";
+import { addPlayerJob, playerHasJob } from "./managers/JobManager.js";
+import { isStart } from "./Party.js";
 
+const dimension = world.getDimension("overworld");
 
 world.events.beforeChat.subscribe( (event) => {
-    if(event.message === "!metier"){
-        world.getDimension("overworld").runCommand("say commande metier exécutée !");
+    event.cancel = true;
+        
+    switch(event.message){
+        case "!metier":
+            if(!isStart){
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] La partie n\'a pas encore commencé"}]}');
+                return;
+            }
+            dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §cChoisissez parmis les métiers ci-dessous :\n§eMineur : §fObtient une pioche en fer efficacité II et les minerais récoltés sont déjà cuit mais possède 7 coeurs\n§eCombattant : §fObtient une épée en fer tranchant 2 mais possède l\'effet lenteur I\n§ePilleur : §fPossède l\'effet faiblesse I et ne prends pas de dégats de chute"}]}');
+            break;
+        case "!metier combattant":
+            if(!playerHasJob(event.sender)){
+                addPlayerJob(event.sender,"combattant");
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez choisis le métier de combattant"}]}');
+            }else{
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez déjà un métier"}]}');
+            }
+            break;
+        case "!metier mineur":
+            if(!playerHasJob(event.sender)){
+                addPlayerJob(event.sender,"mineur");
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez choisis le métier de mineur"}]}');
+            }else{
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez déjà un métier"}]}'); 
+            }
+            break;
+        case "!metier pilleur":
+            if(!playerHasJob(event.sender)){
+                addPlayerJob(event.sender,"pilleur");
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez choisis le métier de pilleur"}]}');
+            }else{
+                dimension.runCommand("tellraw "+ event.sender.name + '{"rawtext":[{"text":"§c[§bMineral Contest§c] §aVous avez déjà un métier"}]}'); 
+            }
+            break;
     }
 });
+
+
--- scripts/Party.js
+++ scripts/Party.js
@@ -2,7 +2,7 @@ import * as GameTest from "mojang-gametest";
 import { Items, ItemStack, Player, World, world, Location} from "mojang-minecraft";
 import {addPlayerInTeam, teams, getPlayerTeam, getSpawnLocationTeam} from "./managers/TeamManager.js";
 
-var isStart = false;
+export var isStart = false;
 var timer = {};
 timer["party"] = 0
 timer["minute"] = 60
@@ -115,7 +115,7 @@ world.events.playerJoin.subscribe((event) => {
         yellowWool.nameTag = "§r§eYellow Team§r§f\n" + teams.yellow.join("\n")
         inventory.setItem(7, yellowWool);
 
-        if(player.name == "Naatof" || player.name == "Achedon"){
+        if(player.name == "Naatof" || player.name == "Achedon12"){
             var compass = new ItemStack(Items.get(`compass`), 1, 4);
             compass.nameTag = "§r§aStart party§f";
             inventory.setItem(8, compass);
